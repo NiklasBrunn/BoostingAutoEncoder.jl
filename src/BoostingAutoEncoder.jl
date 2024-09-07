406346c66de973a34c25f35105021ada6658f14b
NiklasBrunn <niklasbrunn@web.de>
2024-09-07 14:01:54 +0200

    Update module structure

--- src/BoostingAutoEncoder.jl
+++ src/BoostingAutoEncoder.jl
@@ -1,40 +1,46 @@
 module BoostingAutoEncoder
 
-# Write your package code here.
+#--- Used packages ---#
+# Numerical and statistical packages
+using Flux, Random, Statistics, LinearAlgebra, Distances
 
-using Flux;
-using Random; 
-using Statistics;
-using DelimitedFiles;
-using Plots;
-using LinearAlgebra;
-using DataFrames;
-using VegaLite;
-using UMAP;
-using ProgressMeter;
-using ColorSchemes;
-using CSV;
-using Distances;
-using Clustering;
-using RCall
+# Data handling packages
+using DataFrames, CSV, DelimitedFiles
 
+# Plotting and visualization packages
+using Plots, VegaLite, ColorSchemes
+
+# Dimensionality reduction and clustering packages
+using UMAP, Clustering
+
+# Utility packages
+using ProgressMeter, RCall
+
+
+#--- Included files ---#
 include("architecture.jl");
 include("utils.jl");
 include("optimization_functions.jl");
 include("visualization_functions.jl");
 include("simulation_functions.jl");
 
+
+#--- Exported functions ---#
 export 
     # BAE model architecture:
     Hyperparameters, MetaData, BoostingAutoencoder, generate_BAEdecoder,
+
     # BAE model optimization functions:
     get_unibeta, compL2Boost!, disentangled_compL2Boost!, train_BAE!, 
+
     # plotting functions:
     vegaheatmap, vegascatterplot, create_colored_vegascatterplots, TopFeaturesPerCluster_scatterplot, 
     normalizedFeatures_scatterplot, FeaturePlots, origData_FeaturePlots, create_spatialInteractionPlots, track_coefficients, 
     plot_row_boxplots,
+
     # data simulation functions:
     addstages!, sim_scRNAseqData,
+
     # utility functions:
     scale, NaNstoZeros!, Normalize, MinMaxNormalize, log1p, log1pNormalize, filter_observations, 
     filter_features, filter_cells_by_mitochondrial_content, get_latentRepresentation, generate_umap, 
